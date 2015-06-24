26ac289da6bbdea9e8c2079fb6d9cb52c2a74b6e
Justin Williams <justinwilliams42@gmail.com>
2015-06-24 19:44:05 +0100

    remove unused plugins add gulp-inline-source

--- gulpfile.js
+++ gulpfile.js
@@ -1,34 +1,22 @@
 var gulp = require('gulp');
 var del = require('del');
-var concat = require('gulp-concat');
-var rename = require('gulp-rename');
-var uglify = require('gulp-uglify');
-var minifyCss = require('gulp-minify-css');
 var imagemin = require('gulp-imagemin');
-
+var inline = require('gulp-inline-source');
+var sequence = require('gulp-sequence');
 
 gulp.task('clean', function (cb) {
   del(['dist/**/*'], cb);
 });
 
-gulp.task('root-html', function () {
+// ROOT DIR
+
+gulp.task('root:html', function () {
   gulp.src('./*.html')
-  .pipe(gulp.dest('./dist/'));
+    .pipe(inline())
+    .pipe(gulp.dest('./dist/'));
 });
 
-gulp.task('root-css', function () {
-  gulp.src('./css/style.css')
-  .pipe(minifyCss())
-  .pipe(gulp.dest('./dist/css/'));
-});
-
-gulp.task('root-js', function () {
-  gulp.src('./js/*.js')
-  .pipe(uglify())
-  .pipe(gulp.dest('./dist/js/'));
-});
-
-gulp.task('root-images', function () {
+gulp.task('root:images', function () {
   return gulp.src('./img/*')
   .pipe(imagemin({
     progressive: true
@@ -36,35 +24,21 @@ gulp.task('root-images', function () {
   .pipe(gulp.dest('./dist/img/'));
 });
 
-gulp.task('views-html', function () {
+// VIEWS DIR
+
+gulp.task('views:html', function () {
   gulp.src('./views/*.html')
+    .pipe(inline())
     .pipe(gulp.dest('./dist/views/'));
 });
 
-
-gulp.task('views-images', function () {
+gulp.task('views:images', function () {
   return gulp.src('./views/images/*')
-    .pipe(imagemin({
-      progressive: true
-    }))
+    .pipe(imagemin())
     .pipe(gulp.dest('./dist/views/images/'));
 });
 
 
-gulp.task('views-js', function () {
-  return gulp.src('./views/js/*.js')
-    .pipe(uglify())
-    .pipe(gulp.dest('./dist/views/js/'));
-});
-
-
-gulp.task('views-css', function () {
-  return gulp.src('./views/css/*.css')
-    .pipe(minifyCss())
-    .pipe(gulp.dest('./dist/views/css/'));
-});
-
-
-gulp.task('root', ['root-html','root-js','root-css','root-images']);
-gulp.task('views', ['views-html','views-js','views-css','views-images']);
-gulp.task('build', ['root','views']);
+gulp.task('root', ['root:html','root:images']);
+gulp.task('views', ['views:html','views:images']);
+gulp.task('build', sequence('clean',['root','views']));
